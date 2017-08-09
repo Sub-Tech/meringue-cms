9ea004d1190f4a2200ca250428a6c356cadac35d
James Lewis <james@dynamiccarrots.co.uk>
2017-08-09 15:10:14 +0100

    Eek

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,68 +1,124 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet"
+          type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/icons/icomoon/styles.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/core.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/components.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/colors.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/animate.css')}}" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+    <!-- Core JS files -->
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/plugins/loaders/pace.min.js')}}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/core/libraries/jquery.min.js')}}"></script>
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/core/libraries/bootstrap.min.js')}}"></script>
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/plugins/loaders/blockui.min.js')}}"></script>
+    <!-- /core JS files -->
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+    <!-- Theme JS files -->
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/plugins/forms/styling/uniform.min.js')}}"></script>
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/core/app.js')}}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/pages/login.js')}}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/plugins/ui/ripple.min.js')}}"></script>
+    <!-- /theme JS files -->
+
+</head>
+
+<body class="login-container bg-slate-800"  style="background:url('{{ URL::asset('/limitless/assets/images/backgrounds/auth_page.jpg')}}') center center no-repeat; background-size: cover;">
+
+<!-- Page container -->
+<div class="page-container">
+
+    <!-- Page content -->
+    <div class="page-content">
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+            <!-- Content area -->
+            <div class="content">
+
+                <!-- Advanced login -->
+                <form method="POST" action="{{ route('login') }}">
+                    {{ csrf_field() }}
+
+                    <div class="panel panel-body login-form">
+                        <div class="text-center">
+                            <div class="icon-object border-warning-400 text-warning-400"><i class="icon-people"></i>
+                            </div>
+                            <h5 class="content-group-lg">Login In
+                                <small class="display-block">Enter your credentials</small>
+                            </h5>
+                        </div>
+
+
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="text" class="form-control" placeholder="Email Address" name="email"
+                                   value="{{ old('email') }}">
+                            <div class="form-control-feedback">
+                                <i class="icon-mail5 text-muted"></i>
+                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
+                            @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="password" class="form-control" placeholder="Password" name="password"
+                                   value="{{ old('password') }}">
+                            <div class="form-control-feedback">
+                                <i class="icon-lock2 text-muted"></i>
+                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
-                            </div>
+                            @endif
                         </div>
+
+
 
                         <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
-                                    </label>
-                                </div>
-                            </div>
+                            <button type="submit" class="btn bg-pink-400 btn-block">Login <i
+                                        class="icon-circle-right2 position-right"></i></button>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Login
-                                </button>
 
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    Forgot Your Password?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        <div class="content-divider text-muted form-group"><span>Don't have an account?</span></div>
+
+                        <a href="/register" class="btn bg-slate btn-block content-group legitRipple">Register</a>
+
+                    </div>
+                </form>
+                <!-- /advanced login -->
+
             </div>
+            <!-- /content area -->
+
         </div>
+        <!-- /main content -->
+
     </div>
+    <!-- /page content -->
+
 </div>
-@endsection
+<!-- /page container -->
+
+</body>
+</html>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,76 +1,142 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Limitless - Responsive Web Application Kit by Eugene Kopyov</title>
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Register</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
-                        {{ csrf_field() }}
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet"
+          type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/icons/icomoon/styles.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/bootstrap.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/core.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/components.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/colors.css')}}" rel="stylesheet" type="text/css">
+    <link href="{{ URL::asset('/limitless/assets/css/animate.css')}}" rel="stylesheet" type="text/css">
+    <!-- /global stylesheets -->
 
-                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Name</label>
+    <!-- Core JS files -->
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/plugins/loaders/pace.min.js')}}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/core/libraries/jquery.min.js')}}"></script>
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/core/libraries/bootstrap.min.js')}}"></script>
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/plugins/loaders/blockui.min.js')}}"></script>
+    <!-- /core JS files -->
 
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+    <!-- Theme JS files -->
+    <script type="text/javascript"
+            src="{{ URL::asset('/limitless/assets/js/plugins/forms/styling/uniform.min.js')}}"></script>
 
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/core/app.js')}}"></script>
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/pages/login.js')}}"></script>
+
+    <script type="text/javascript" src="{{ URL::asset('/limitless/assets/js/plugins/ui/ripple.min.js')}}"></script>
+    <!-- /theme JS files -->
+
+</head>
+
+<body class="login-container bg-slate-800"  style="background:url('{{ URL::asset('/limitless/assets/images/backgrounds/auth_page.jpg')}}') center center no-repeat; background-size: cover;">
+
+<!-- Page container -->
+<div class="page-container">
+
+    <!-- Page content -->
+    <div class="page-content">
+
+        <!-- Main content -->
+        <div class="content-wrapper">
+
+            <!-- Content area -->
+            <div class="content">
+
+                <!-- Advanced login -->
+                <form method="POST" action="{{ route('register') }}" class="animated jackInTheBox">
+                    {{ csrf_field() }}
+
+                    <div class="panel panel-body login-form">
+                        <div class="text-center">
+                            <div class="icon-object" style="background:#333333; padding:9px;">
+                                <img width="60" src="{{ URL::asset('/limitless/assets/images/cake.png')}}">
+                            </div>
+                            <h5 class="content-group-lg">Create New Account
+                                <small class="display-block">Enter your credentials</small>
+                            </h5>
+                        </div>
+
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="text" class="form-control" placeholder="First Name" name="name"
+                                   value="{{ old('name') }}">
+                            <div class="form-control-feedback">
+                                <i class="icon-user text-muted"></i>
+                            </div>
+                            @if ($errors->has('name'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('name') }}</strong>
                                     </span>
-                                @endif
-                            </div>
+                            @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="text" class="form-control" placeholder="Email Address" name="email"
+                                   value="{{ old('email') }}">
+                            <div class="form-control-feedback">
+                                <i class="icon-mail5 text-muted"></i>
+                            </div>
+                            @if ($errors->has('email'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
                                     </span>
-                                @endif
-                            </div>
+                            @endif
                         </div>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="password" class="form-control" placeholder="Password" name="password"
+                                   value="{{ old('password') }}">
+                            <div class="form-control-feedback">
+                                <i class="icon-lock2 text-muted"></i>
+                            </div>
+                            @if ($errors->has('password'))
+                                <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
                                     </span>
-                                @endif
+                            @endif
+                        </div>
+
+                        <div class="form-group has-feedback has-feedback-left">
+                            <input type="password" class="form-control" placeholder="Confirm Password" name="password_confirmation">
+                            <div class="form-control-feedback">
+                                <i class="icon-lock2 text-muted"></i>
                             </div>
                         </div>
+
 
                         <div class="form-group">
-                            <label for="password-confirm" class="col-md-4 control-label">Confirm Password</label>
-
-                            <div class="col-md-6">
-                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
-                            </div>
+                            <button type="submit" class="btn bg-pink-400 btn-block">Register <i
+                                        class="icon-circle-right2 position-right"></i></button>
                         </div>
 
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Register
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+                        <div class="content-divider text-muted form-group"><span>Already have an account?</span></div>
+
+                        <a href="/login" class="btn bg-slate btn-block content-group legitRipple">Login</a>
+
+                    </div>
+                </form>
+                <!-- /advanced login -->
+
             </div>
+            <!-- /content area -->
+
         </div>
+        <!-- /main content -->
+
     </div>
+    <!-- /page content -->
+
 </div>
-@endsection
+<!-- /page container -->
+
+</body>
+</html>
