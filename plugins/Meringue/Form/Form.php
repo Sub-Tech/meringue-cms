38c205a13511ea9974ef9e19c6de53843a969a32
Jaden <jaden.shepherd@submissiontechnology.co.uk>
2017-08-15 12:35:06 +0100

    Form Plugin and autoloader

--- plugins/Meringue/Form/Form.php
+++ plugins/Meringue/Form/Form.php
@@ -4,6 +4,8 @@ namespace Plugins\Meringue\Form;
 
 use App\PluginBase;
 use App\PluginInterface;
+use Illuminate\Http\Request;
+use Validator;
 
 /**
  * Class Form
@@ -29,7 +31,7 @@ class Form extends PluginBase implements PluginInterface
      *
      * @return array
      */
-    public function setDetails(): array
+    public function details(): array
     {
         return [
             'name' => 'Form',
@@ -59,23 +61,42 @@ class Form extends PluginBase implements PluginInterface
 
 
     /**
+     * Handle form submission
      *
+     * @param Request $request
+     * @return \Illuminate\Database\Eloquent\Model
      */
-    public function render()
+    public function handleAjax(Request $request)
     {
-        require __DIR__ . '/Models/Form.php';
-        require __DIR__ . '/Models/Input.php';
+        $form = Models\Form::find($request->form_id);
 
-        return view('Meringue/Form/views/form', [
-            'form' => Models\Form::find(1)
-        ]);
+        Validator::validate($request->all(), $form->validation);
+
+        $tits = $form->responses()->create($request->all());
+
+        return response()->json($tits);
     }
 
 
     /**
-     * Install the plugin
+     * Route begins from the plugins/ folder
+     * Must return view('merchant/plugin/views/viewName) or equivalent
+     * Return false if plugin doesn't need to render anything on the front end
      *
-     * @return bool|void
+     * @return \Illuminate\View\View|\Illuminate\Contracts\View\Factory|bool
+     */
+    public function render()
+    {
+        return view('Meringue/Form/views/form')
+            ->with('form', Models\Form::find(1)); // TODO test id -> make dynamic
+    }
+
+
+    /**
+     * Runs any method that need to be ran upon installation of the Plugin
+     * Return false if not necessary
+     *
+     * @return void|bool
      */
     public function install()
     {
---
+++ plugins/Meringue/Form/autoload.php
@@ -0,0 +1,4 @@
+<?php
+
+require __DIR__ . '/Models/Form.php';
+require __DIR__ . '/Models/Input.php';
