d5dbbff7f53175f8e8a11df81660064fc60b50b8
Jaden <jaden.shepherd@submissiontechnology.co.uk>
2017-08-21 15:31:40 +0100

    Text block now displays content

---
+++ plugins/Meringue/Text/database/migrations/2017_08_03_145738_create_meringue_text_text_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTextTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('meringue_text_text', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('meringue_text_text');
+    }
+}
