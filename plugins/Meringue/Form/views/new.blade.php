211ce1cdc5542cec17914d9934f39d3317b7b394
Jaden <jaden.shepherd@submissiontechnology.co.uk>
2017-08-21 15:27:47 +0100

    Make the new Form page look a bit better

--- plugins/Meringue/Form/views/new.blade.php
+++ plugins/Meringue/Form/views/new.blade.php
@@ -1,13 +1,21 @@
 @extends('admin.app')
 
 @section('content')
-    <form action="{{ route('Form.store')}}" method="POST">
-
-        <div class="form-group">
-            <label for="name">Form Name</label>
-            <input class="form-control" type="text" name="name" id="name">
+    <div class="panel panel-flat">
+        <div class="panel-heading">
+            <h3>Create New Form</h3>
         </div>
 
-        <input class="btn btn-primary" type="submit" value="Submit">
-    </form>
+        <div class="panel-body">
+            <form action="{{ route('Form.store')}}" method="POST">
+
+                <div class="form-group">
+                    <label for="name">Form Name</label>
+                    <input class="form-control" type="text" name="name" id="name">
+                </div>
+
+                <input class="btn btn-primary" type="submit" value="Submit">
+            </form>
+        </div>
+    </div>
 @endsection
