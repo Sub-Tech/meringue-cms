baed9adf84201a3b9ec0490ea9c10f7d2467c366
Jaden <jaden.shepherd@submissiontechnology.co.uk>
2017-08-23 11:11:01 +0100

    Better way to do the below

--- app/Renderers/BlockRenderer.php
+++ app/Renderers/BlockRenderer.php
@@ -14,6 +14,7 @@ class BlockRenderer
 
     /**
      * Open the section
+     *
      * @param Block $block
      * @return string (of HTML)
      */
@@ -21,13 +22,26 @@ class BlockRenderer
     {
         $plugin = PluginInitialiser::getPlugin($block->plugin_class);
 
-        try {
-            return "<div class='block col-md-{$block->width}'>" .
-                $plugin->render($block->instance_id) .
-                "</div>";
-        } catch (\Exception $exception) {
+        if ($this->blockRequiresInstanceOf($plugin) && is_null($block->instance_id)) {
             return "<span style='color:red'>Content not set for block {$block->id}!</span>";
         }
+
+        return "<div class='block col-md-{$block->width}'>" .
+            $plugin->render($block->instance_id) .
+            "</div>";
+    }
+
+
+    /**
+     * Checks to see if the Plugin requires an instance
+     * or: see if it implements the Instance Interface
+     *
+     * @param $plugin
+     * @return bool
+     */
+    private function blockRequiresInstanceOf($plugin)
+    {
+        return in_array('App\InstanceInterface', class_implements($plugin));
     }
 
 }
