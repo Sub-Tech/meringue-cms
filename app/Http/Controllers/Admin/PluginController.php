14d5d7011c9a8482e865f846a891f93c2f33bb72
Jaden <jaden.shepherd@submissiontechnology.co.uk>
2017-09-01 12:00:49 +0100

    Mess around with the new Responsable Interface to neaten up the PluginController

--- app/Http/Controllers/Admin/PluginController.php
+++ app/Http/Controllers/Admin/PluginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Plugin\PluginInitialiser;
 use App\Http\Controllers\Controller;
 use App\Plugin;
+use App\Responses\AjaxResponse;
 use Illuminate\Http\Request;
 
 /**
@@ -47,37 +48,29 @@ class PluginController extends Controller
      * Activates the Plugin
      *
      * @param Request $request
-     * @return \Illuminate\Http\JsonResponse
+     * @return AjaxResponse
      */
     public function activate(Request $request)
     {
         $plugin = Plugin::find($request->plugin);
 
         if ($plugin->active) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Plugin already activated'
-            ]);
+            return new AjaxResponse($message = 'Plugin already activated', $success = false);
         }
 
         if (!$plugin->installed) {
             try {
                 PluginInitialiser::getPlugin($plugin->class_name)->install();
+                $plugin->installed = 1;
             } catch (\Exception $e) {
-                return response()->json([
-                    'success' => false,
-                    'message' => $e->getMessage()
-                ]);
+                return new AjaxResponse($message = $e->getMessage(), $success = false);
             }
-            $plugin->installed = 1;
         }
 
         $plugin->active = 1;
         $plugin->save();
 
-        return response()->json([
-            'success' => true
-        ]);
+        return new AjaxResponse($message = 'Activation successful', $success = true);
     }
 
 }
---
+++ app/Responses/AjaxResponse.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Responses;
+
+use Illuminate\Contracts\Support\Responsable;
+
+/**
+ * Class Tits
+ */
+class AjaxResponse implements Responsable
+{
+
+    /**
+     * @var string
+     */
+    private $message = '';
+
+    /**
+     * @var bool|string
+     */
+    private $success = '';
+
+
+    /**
+     * Tits constructor.
+     * @param string $message
+     * @param bool $success
+     */
+    public function __construct(string $message, bool $success)
+    {
+        $this->message = $message;
+        $this->success = $success;
+    }
+
+
+    /**
+     * Create an HTTP response that represents the object.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function toResponse($request)
+    {
+        return response()->json([
+            'message' => $this->message,
+            'success' => $this->success
+        ]);
+    }
+
+}
