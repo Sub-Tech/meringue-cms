5bb6c3f9a0c147827986288eb8b1fe550f5fb74a
James Lewis <james@dynamiccarrots.co.uk>
2017-09-06 10:01:14 +0100

    Tarting it up

--- plugins/Meringue/Form/views/new.blade.php
+++ plugins/Meringue/Form/views/new.blade.php
@@ -1,21 +1,27 @@
 @extends('admin.app')
 
 @section('content')
-    <div class="panel panel-flat">
-        <div class="panel-heading">
-            <h3>Create New Form</h3>
-        </div>
-
-        <div class="panel-body">
-            <form action="{{ route('Form.store')}}" method="POST">
-
-                <div class="form-group">
-                    <label for="name">Form Name</label>
-                    <input class="form-control" type="text" name="name" id="name">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h6 class="panel-title">Create New Form</h6>
+                    <div class="heading-elements">
+                        <!-- Space For Buttons -->
+                    </div>
                 </div>
 
-                <input class="btn btn-primary" type="submit" value="Submit">
-            </form>
+                <div class="panel-body">
+                    <form action="{{ route('Form.store')}}" method="POST">
+
+                        <div class="form-group">
+                            <label for="name">Form Name</label>
+                            <input class="form-control" type="text" name="name" id="name">
+                        </div>
+
+                        <input class="btn btn-primary" type="submit" value="Submit">
+                    </form>
+                </div>
+            </div>
         </div>
-    </div>
 @endsection
