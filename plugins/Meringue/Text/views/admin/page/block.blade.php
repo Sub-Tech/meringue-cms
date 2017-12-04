f4a9e611de1903afb7cb3f01a0313f0b8a460cf3
Daniel Rogers <daniel.rogers@submissiontechnology.co.uk>
2017-12-04 12:27:05 +0000

    Added Text editor for content, added font awesome icons.

--- plugins/Meringue/Text/views/admin/page/block.blade.php
+++ plugins/Meringue/Text/views/admin/page/block.blade.php
@@ -1,17 +1,17 @@
 @if (isset($block->instance_id))
-    <div id="editor1" contenteditable="true">
+    <div id="editor<?= $block->instance_id; ?>" contenteditable="true">
         {!! $block->plugin->class()->getInstance($block->instance_id)->content !!}
     </div>
 @endif
 
 <script>
     $('body').click(function () {
-        if ($('#editor1').hasClass('cke_focus')) {
+        if ($('#editor<?= $block->instance_id; ?>').hasClass('cke_focus')) {
             $.ajax({
                 method: 'patch',
                 url: '/admin/instances/{{ $block->instance_id }}',
                 data: {
-                    content: CKEDITOR.instances.editor1.getData(),
+                    content: CKEDITOR.instances.editor<?= $block->instance_id; ?>.getData(),
                     vendor: '{{ $block->plugin->class()->getVendor() }}',
                     plugin: '{{ $block->plugin->class()->getName() }}'
                 }
